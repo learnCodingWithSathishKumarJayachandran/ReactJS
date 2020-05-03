b3bbd626287d5e0dfdf25f1234955e99b702a2be
learnCodingWithSathishKumarJayachandran <64748630+learnCodingWithSathishKumarJayachandran@users.noreply.github.com>
2020-05-03 14:50:47 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+//import ClasLogin from './Tutorial17/ClassLogin'
+//import Login from './Tutorial20/Login'
+//import ConditionalRendering from './Tutorial22/ConditionalRendering'
+//import RenderingList from './Tutorial24/RenderingList'
+//import caca from './Tutorial24/RenderingList'
+//import StudentList from './Tutorial24/StudentList'
+//import Style from './Tutorial30/Style'
+//import ParentComp from './Tutorial38/ParentComponent'
+//import ParentCompo from './ComponentLifeCycle/ParentComponent'
+import Parent from './ComponentLifeCyclePa/ParentComponent'
+
+
+//import Forms from './Tutorial34/Forms'
+
+
+function App() {
+  return (
+    <div>
+         <Parent/>
+      </div>
+      
+  )
+}
+
+export default App;
