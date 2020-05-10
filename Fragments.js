9cfaf2dda861554475c84974c089f0ef5b940891
learnCodingWithSathishKumarJayachandran <64748630+learnCodingWithSathishKumarJayachandran@users.noreply.github.com>
2020-05-09 20:15:26 -0400

    Add files via upload

---
+++ Fragments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Table from './Table'
+
+function Fragments(props) {
+    return (
+        <React.Fragment>
+        <h1>
+            Fragement Demo
+        </h1>
+        <p>This is a Fragment Description</p>
+        <Table/>
+        </React.Fragment>
+        
+    );
+}
+
+export default Fragments;
